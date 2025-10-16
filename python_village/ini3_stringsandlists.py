d8bc32ac6f505f501b671dbfee1ac6acb184646e
bunnyswan <bunsree@gmail.com>
2025-10-16 19:10:24 -0400

    ini2_variablesandsomearithmetic.py

---
+++ python_village/ini3_stringsandlists.py
@@ -0,0 +1,24 @@
+user_string = "HumptyDumptysatonawallHumptyDumptyhadagreatfallAlltheKingshorsesandalltheKingsmenCouldntputHumptyDumptyinhisplaceagain"
+
+a = 22
+b = 27
+c = 97
+d = 102
+
+first_part = user_string[a:b+1]
+second_part = user_string[c:d+1]
+
+print(first_part, second_part)
+
+user_string = (
+    "i3yiUy33wO9Ze0muK7oK9R3lt8NlBzYe5ip73quDEx8JJajhdnygfG8HZQwsxCircaetus4BYhEVxsNMJcm6Sk1mGaqOxoLpG9sU"
+    "JKR0bJgi1cdahuricaZWnb4SGlbUV8OKalQy3eeh57tlPwWl9EMoLX37U8oTDPm5Xtpv5zUyH9r3rYrlCydYjCd57KIbX"
+)
+a = 61
+b = 69
+c = 110
+d = 117
+first_part = user_string[a:b+1]
+second_part = user_string[c:d+1]
+
+print(first_part, second_part)
