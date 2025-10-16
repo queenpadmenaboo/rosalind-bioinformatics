d18a2f377e55b78408021f385de8969b1b4550f9
bunnyswan <bunsree@gmail.com>
2025-10-16 12:48:48 -0400

    Add first Rosalind solution: INI1

---
+++ python_village/ini1_introduction.py
@@ -0,0 +1,14 @@
+"""
+Rosalind Problem: INI1 - Installing Python
+Problem URL: http://rosalind.info/problems/ini1/
+
+Author: Bunsree Patel
+Date: October 16, 2025
+"""
+
+def main():
+    """Display the Zen of Python"""
+    import this
+
+if __name__ == "__main__":
+    main()
