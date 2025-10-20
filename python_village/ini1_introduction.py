2426148623562ad99bede92e65563435c6acb609
bunnyswan <bunsree@gmail.com>
2025-10-20 13:46:55 -0400

    ini1_introduction.py

--- python_village/ini1_introduction.py
+++ python_village/ini1_introduction.py
@@ -5,6 +5,14 @@ Problem URL: http://rosalind.info/problems/ini1/
 Author: Bunsree Patel
 Date: October 16, 2025
 """
+"""Python is a popular programming language known for its simplicity and readability.
+It is widely used in various fields, including web development, data analysis, artificial intelligence, scientific computing, and more.
+Launch IDLE (Integrated Development and Learning Environment) to start writing and executing Python code.
+>>> are Python's way of saying that what follows is a command to be executed in the Python interpreter.
+Any command entered at the prompt will be executed immediately, and the result (if any) will be displayed."""
+
+print(1 + 1)
+print('Hello, World!') 
 
 def main():
     """Display the Zen of Python"""
@@ -12,3 +20,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+"""To add, commit, and push files to GitHub repository, use the following commands:
+
+git add .
+git commit -m "File name i.e. ini1_introduction.py"
+git push
+"""
