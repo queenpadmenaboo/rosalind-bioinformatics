7600fbbb7b4b7b16c5e2ac1817da200d865a035e
bunnyswan <bunsree@gmail.com>
2025-10-21 13:45:30 -0400

    ini2_variablesandsomearithmetic.py

--- python_village/ini2_variablesandsomearithmetic.py
+++ python_village/ini2_variablesandsomearithmetic.py
@@ -49,7 +49,10 @@ c = '!'
 print(a + ' ' + b + c*3)
 
 
-"""Sample Dataset"""
+"""Sample Dataset Problem
+Given: Two positive integers a and b, each less than 1000.
+Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b."""
+
 a = 3
 b = 5
 c = ( a ** 2 + b ** 2 )
