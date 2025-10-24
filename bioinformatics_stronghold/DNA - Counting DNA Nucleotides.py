63c649c5d0f589b3231a2507576f460af0ed1361
bunnyswan <bunsree@gmail.com>
2025-10-24 16:14:04 -0400

    DNA - Counting DNA Nucleotides.py

---
+++ bioinformatics_stronghold/DNA - Counting DNA Nucleotides.py
@@ -0,0 +1,22 @@
+"""
+Rosalind Problem: DNA - Counting DNA Nucleotides
+Problem URL: http://rosalind.info/problems/dna/
+
+Author: Bunsree Patel
+Date: October 24, 2025
+"""
+
+"""DNA (deoxyribonucleic acid) is a molecule that carries the genetic instructions used in the growth, development, functioning, and reproduction of all known living organisms and many viruses. 
+DNA is composed of four types of nucleotides, adenine (A), cytosine (C), guanine (G), and thymine (T). In RNA, uracil (U) replaces thymine.
+
+The nucleic acid monomer is called a nucleotide (abbreaviated to nt), and is used as a unit of strand length. 
+Each nucleotide consists of a sugar molecule, a negatively charged phosphate group, and a nitrogenous base (nucleobase)
+A sugar-phosphate backbone is formed by covalent bonds between the sugar of one nucleotide and the phosphate of the next nucleotide in the chain.
+Nucleotides of a specific type of nucleic acid (DNA or RNA) differ only in the nitrogenous base they contain.
+
+The ordering of bases defines a nucleic acid's primary structure.
+
+DNA is fcound in all living organisms on Earth, including bacteria, and many viruses (which are often considered to be nonliving).
+Genome refers to the sum total of the DNA in an organism's chromosomes."""
+
+
