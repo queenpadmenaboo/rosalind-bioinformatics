f9987146d88c663b0d21c27de015c2ef1b21ac2e
bunnyswan <bunsree@gmail.com>
2025-10-24 17:26:51 -0400

    main.py

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -10,3 +10,9 @@ def validateSeq(dna_seq):
         if nuc not in Nucleotides:
             return False
     return tmpseq
+
+def countNucFrequency(seq):
+    tmpFreqDict = {"A": 0, "C": 0, "G": 0, "T": 0}
+    for nuc in seq:
+        tmpFreqDict[nuc] += 1
+    return tmpFreqDict
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 # DNA Toolset/Code testing file
-from DNAToolkit import *
+"""Testing validateSeq function from DNAToolkit.py"""
+from DNAToolkit import validateSeq
 rndDNAStr = "ATTTCGT"
-
 print(validateSeq(rndDNAStr))
+# Expected Output: ATTTCGT
+
+"""Testing validateSeq function from DNAToolkit.py, invalid character 'X' in string."""
+from DNAToolkit import validateSeq
+rndDNAStr = "ATTTCGTX"
+print(validateSeq(rndDNAStr))
+# Expected Output: False
+
+"""Mixed case (upper and lower case) letters should give string output in all upper case."""
+from DNAToolkit import validateSeq
+rndDNAStr = "AtCCgGGtGGt"
+print(validateSeq(rndDNAStr))
+# Expected Output: ATCCGGGTGGT
+
+"""All 3 testing scenarios passed successfully."""
+
+
+from DNAToolkit import *
+import random
+
+# Creating a random DNA string of length 20
+randDNAStr = ''.join([random.choice(Nucleotides)
+                     for nuc in range(50)])
+print(validateSeq(randDNAStr))
+
+print(validateSeq(randDNAStr))
+print(countNucFrequency(randDNAStr))
