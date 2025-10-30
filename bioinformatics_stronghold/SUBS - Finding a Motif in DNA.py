645e845698b7e36c38d56ae3e1a7dc1b058221ec
queenpadmenaboo <bunsree@gmail.com>
2025-10-30 12:10:47 -0400

    SUBS - Finding a Motif in DNA

--- bioinformatics_stronghold/SUBS - Finding a Motif in DNA.py
+++ bioinformatics_stronghold/SUBS - Finding a Motif in DNA.py
@@ -19,11 +19,10 @@ Alu, however, has not been found to serve a positive purpose, and appears in fac
 
 """Sample Dataset Problem
 Given two strings s and t, t is a substring of s if t is contained as a contiguous collection of symbols in s (as a result, t must be no longer than s).
-
-The position of a symbol in a string is the total number of symbols found to its left, including itself (e.g., the positions of all occurrences of 'U' in "AUGCUUCAGAAAGGUCUUACG" are 2, 5, 6, 15, 17, and 18). The symbol at position i of s is denoted by s[i].
+The position of a symbol in a string is the total number of symbols found to its left, including itself (e.g., the positions of all occurrences of 'U' in "AUGCUUCAGAAAGGUCUUACG" are 2, 5, 6, 15, 17, and 18). 
+The symbol at position i of s is denoted by s[i].
 
 A substring of s can be represented as s[j:k], where j and k represent the starting and ending positions of the substring in s; for example, if s = "AUGCUUCAGAAAGGUCUUACG", then s[2:5] = "UGCU".
-
 The location of a substring s[j:k] is its beginning position j; note that t will have multiple locations in s if it occurs more than once as a substring of s (see the Sample below).
 
     Given: Two DNA strings s and t (each of length at most 1 kbp).
@@ -41,4 +40,14 @@ result = find_substring_locations(s,t)
 
 # Format and print output
 print(*result)
-"""Output: 2 4 10"""
+"""Output: 2 4 10. The '*' is the unpacking operator. 
+It takes a list and spreads out its elements as separate arguments."""
+
+"""Actual Dataset Problem"""
+
+s = "CTATAAGAATGATGGCACTATAAGAACTCATTGGTCTTCTATAAGGCCTATAAGACGCTATAAGCCTATAAGCTATAAGATAACCTATAAGCTATAAGGCTATAAGCCTATAAGCCTATAAGACACTATAAGCCCTAACTATAAGCTATAAGCTATAAGGTCTATAAGCTATAAGCTATAAGGCTCTATAAGAAAGCCTATAAGCTATAAGCTATAAGACTATAAGGACTATAAGCCTATAAGCTATAAGGGCTATAAGCTCTATAAGTGGACCCTATAAGTGTTCTATAAGCTATAAGGGGCTATAAGCCTATAAGACGAAGCTATAAGTGTAGCTATAAGCTATAAGCACTATAAGCTATAAGAACTATAAGCTATAAGCTATAAGGGCTATAAGCTATAAGCTATAAGCTATAAGGGGGGGCCGCTATAAGCTATAAGGGGGATCTATAAGACTATAAGCCTATAAGCAACTATAAGTTCTATAAGGGCGCTATAAGCCTATAAGAGTGCAGCTATAAGAACTATAAGTGCTATAAGACTATAAGAGAGCCGGCTATAAGTCCTATAAGGTGCCTATAAGACTATAAGTCTATAAGATGCGGCACTATAAGTGCTTATCGCTATAAGCCTATAAGCTATAAGGCAAACCTATAAGCGGTTACTATAAGTAAGTACTATAAGGCTATAAGCATCTATAAGATCACTATAAGCTATAAGTTCATCACCTGTCTATAAGAAACCCTATAAGCTATAAGCCTATAAGCGCTATAAGTTCTATAAGGATACTATAAGTATCGCTATAAGTCCTATAAGCTATAAGACTATAAGCCCTATAAGTCGTCTATAAGGTCCAACTATAAGCTATAAGGCTATAAGGCTATAAGGCTCCTATAAGGCCTTACTATAAGCCTATAAGCTATAAGCTATAAGACTATAAGGGTGCTACTATAAG"
+t = "CTATAAGCT"
+
+result = find_substring_locations(s,t)
+print(*result)
+"""Output: 66 85 139 146 162 169 198 205 237 253 286 336 352 368 375 391 398 405 428 632 707 745 810 858 913 920"""
