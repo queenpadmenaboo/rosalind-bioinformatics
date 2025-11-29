591016e5556dd059c14e9b539008ad158be6de26
queenpadmenaboo <bunsree@gmail.com>
2025-11-28 23:40:29 -0500

    updated FRMT

--- bioinformatics_armory/FRMT - Data Formats.py
+++ bioinformatics_armory/FRMT - Data Formats.py
@@ -38,4 +38,4 @@ from Bio import Entrez
 Entrez.email = "bunsree@gmail.com"
 handle = Entrez.efetch(db="nucleotide", id=["FJ817486, JX069768, JX469983"], rettype="fasta")
 records = handle.read()
-print records
+print(records)
