a669f6c3de6aa5fad2310b9a6e4b99b388c45952
queenpadmenaboo <bunsree@gmail.com>
2025-11-30 18:13:46 -0500

    updated solution

--- bioinformatics_armory/convert.py
+++ bioinformatics_armory/convert.py
@@ -1,2 +1,4 @@
 from Bio import SeqIO
 SeqIO.convert("test.fastq", "fastq", "test.fasta", "fasta")
+
+# Output will be in 'test.fasta' file under same file structure as 'test.fastq' and 'convert.py'
